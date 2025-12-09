e67898b546f33455832bc7af284c46aad9e55941
RajeshPandey10 <imrajesh2005@gmail.com>
2025-12-09 17:01:51 +0545

    card section

--- day3/src/componetns/products/ProductCard.jsx
+++ day3/src/componetns/products/ProductCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import Button from '../common/Button'
 
 const ProductCard = ({product}) => {
   return (
     <>
-    <div className='h-50 '>
+    <div className='border border-amber-300 bg-amber-100 px-4 py-4  rounded-lg'>
 
-        <img src={product.img} alt="" />
+        <img src={product.img} alt="" className='rounded-lg' />
 
-        <p className='text-black'>{product.price}</p>
+        <p className='text-black'>${product.price}</p>
        <h2 className='font-extrabold'>{product.name}</h2>
        <p className='text-gray-400'>{product.brand}</p>
-
+     <Button text="Buy now"/>
 
     </div>
     
